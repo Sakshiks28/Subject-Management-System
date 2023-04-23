d658b3d30425251b528c27b3b45d89ff19d263a5
Sakshiks28 <123305244+Sakshiks28@users.noreply.github.com>
2023-04-23 22:29:12 +0530

    Add files via upload
    Spring Boot Project  :-  Subject Management System

---
+++ SpringBootCrudEx/src/main/java/com/anudip/subjectmanagement/repository/SubjectRepository.java
@@ -0,0 +1,9 @@
+package com.anudip.subjectmanagement.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.anudip.subjectmanagement.entity.Subject;
+
+public interface SubjectRepository extends CrudRepository<Subject, String>{
+
+}
